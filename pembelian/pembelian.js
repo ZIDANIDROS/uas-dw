2f42d047a2b76fa608c9aab1d524ed3e2cfed6aa
husein zidan <97558377+ZIDANIDROS@users.noreply.github.com>
2023-12-26 00:34:42 +0700

    Add files via upload

---
+++ pembelian/pembelian.js
@@ -0,0 +1,7 @@
+document.getElementById('cart').addEventListener('click', function () {
+  alert('sukses masuk ke keranjang');
+});
+
+document.getElementById('buy').addEventListener('click', function () {
+  alert('login terlebih dahulu');
+});
